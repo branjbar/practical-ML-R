43f3201322cc47f3fc71fa1754b16f5b978d25c7
Bijan Ranjbar <bij.ranjbar@gmail.com>
2015-08-10 21:40:13 +0200

    exploring data and choosing 21 important features

--- project/main.R
+++ project/main.R
@@ -11,23 +11,54 @@ library(caret)
 
 # load data
 myData <- read.csv("/Users/bian/sandbox/practical-ML-R/project/pml-training.csv")
+validationData <- read.csv("/Users/bian/sandbox/practical-ML-R/project/pml-testing.csv")
 
 # split data into training and test sets
-inTrain <- createDataPartition(y=myData$classe, p=0.8, list=FALSE)
+inTrain <- createDataPartition(y=myData$classe, p=0.3, list=FALSE)
 training <- myData[inTrain,]
 testing <- myData[-inTrain,]
 
+##### TRY1 Exploring Data (accuracy 60%)
+featurePlot(x=training[,8:11],
+            y=training$classe,
+            plot = "pairs")
+modelFit <- train(classe ~ pitch_belt + roll_belt,
+              method="ctree", #ctree, rpart, gbm, rf 
+              data=training) 
+modelFit  
 
-# I start with a predictive model with three covariates
-modelFit <- train(classe ~ roll_arm + pitch_arm + yaw_arm,
-              method="rf", #ctree, rpart, gbm 
-              data=training)
 
-modelFit  # accuracy is 0.22
+##### TRY2: Exploring Data (accuracy 70%)
+featurePlot(x=training[,10:14],
+            y=training$classe,
+            plot = "pairs")
+modelFit <- train(classe ~ pitch_belt + roll_belt + yaw_belt + total_accel_belt,
+                  method="ctree", #ctree, rpart, gbm, rf --> (accuracy=60%)
+                  data=training) 
+modelFit  
 
+##### TRY3: Exploring Data (accuracy 70%)
+featurePlot(x=training[,116:120],
+            y=training$classe,
+            plot = "pairs")
+# training$stddev_yaw_arm_log <- log10(training$stddev_yaw_arm+1) #dealing with the skewed distribution
+modelFit <- train(classe ~ pitch_belt + roll_belt + yaw_belt + total_accel_belt + 
+                    gyros_belt_y + gyros_belt_z + accel_belt_y +
+                    magnet_belt_y + roll_arm + pitch_arm + yaw_arm + avg_roll_arm + accel_arm_x +
+                    avg_roll_dumbbell + gyros_dumbbell_x + magnet_dumbbell_x + accel_dumbbell_y + 
+                    
+                    ,
+                  method="ctree", #ctree, rpart, gbm, rf --> (accuracy=60%),
+                  preProcess="pca",
+                  data=training); modelFit  
+
+
+###### testset ######
 confusionM <- confusionMatrix(predict(modelFit,testing), testing$classe)  
+confusionM
 confusionM$table
 plot(confusionM$table)
+
 
 
 
