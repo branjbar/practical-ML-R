d44f66accfd195c4909e293809a600573047d1b4
Bijan Ranjbar <bij.ranjbar@gmail.com>
2015-08-10 17:44:50 +0200

    correcting a typo

--- week2_part4_covariates.r
+++ week2_part4_covariates.r
@@ -9,7 +9,7 @@ training <- Wage[inTrain,]; testing <- Wage[-inTrain,]
 
 # convert factor variables to indicator variables
 table(training$jobclass)
-dummies <- dummyVars(wage ~ jobclass, data=training)# the outcome is wage and jobclass  is the predicted variable.  
+dummies <- dummyVars(wage ~ jobclass, data=training) # the outcome is wage and jobclass  is the predictor variable.  
 
 head(predict(dummies,newdata=training))
 
@@ -23,7 +23,7 @@ library(splines)
 bsBasis <- bs(training$age, df=3)  # fit a polynomial to age
 lm1 <- lm(wage ~ bsBasis, data=training)  # 
 plot(training$age, training$wage, pch=19, cex=0.5)  # pch for marker and cex for scale
-points(training$age,predict(lm1,newdata=training),col="red",pch=19,cex=0.5) plotting the fit spline
+points(training$age,predict(lm1,newdata=training),col="red",pch=19,cex=0.5) # plotting the fit spline
 
 # splines on the test set
 predict(bsBasis, age=testing$age)  # please note that we're using the same bsBasis which is fit to the training data
