96fc5423876061b7998deb27c470ef585152c799
Bijan Ranjbar <bij.ranjbar@gmail.com>
2015-08-11 10:07:09 +0200

    first working version
    na.strings added to data import, tries are removed and useful features
    are clarified. pca and knnimpute are used.

--- project/main.R
+++ project/main.R
@@ -5,59 +5,75 @@
 # lifting the dumbbell only halfway (Class C), lowering the dumbbell only halfway (Class D) 
 # and throwing the hips to the front (Class E).
 
+# here are sample solutions of the others:
+# https://rpubs.com/jocaqui/26619
+# https://github.com/jeffheaton/jhi-practical-machine-learning/blob/master/PracticalMachineLearning.RMD
+# http://emres.github.io/practical-ml/
+# http://rstudio-pubs-static.s3.amazonaws.com/20131_7fca728506724c27bc629081983f0ec2.html
+
+
 # initialization
 set.seed(3421)
 library(caret)
 
 # load data
-myData <- read.csv("/Users/bian/sandbox/practical-ML-R/project/pml-training.csv")
-validationData <- read.csv("/Users/bian/sandbox/practical-ML-R/project/pml-testing.csv")
+setwd("/Users/bian/sandbox/practical-ML-R/project/")
+myData <- read.csv("pml-training.csv", header=T,  na.strings=c("NA", "#DIV/0!"))
+validationData <- read.csv("pml-testing.csv")
 
 # split data into training and test sets
-inTrain <- createDataPartition(y=myData$classe, p=0.3, list=FALSE)
+inTrain <- createDataPartition(y=myData$classe, p=1, list=FALSE)
 training <- myData[inTrain,]
 testing <- myData[-inTrain,]
 
-##### TRY1 Exploring Data (accuracy 60%)
-featurePlot(x=training[,8:11],
-            y=training$classe,
-            plot = "pairs")
-modelFit <- train(classe ~ pitch_belt + roll_belt,
-              method="ctree", #ctree, rpart, gbm, rf 
-              data=training) 
-modelFit  
+###### Explore Data and choose useful features ######
+useful_features = c("pitch_belt","roll_belt","yaw_belt","total_accel_belt","gyros_belt_y",
+                    "gyros_belt_z","accel_belt_y","magnet_belt_y","roll_arm","pitch_arm",
+                    "yaw_arm","avg_roll_arm","accel_arm_x","avg_roll_dumbbell","gyros_dumbbell_x",
+                    "magnet_dumbbell_x","accel_dumbbell_y","roll_forearm", "magnet_dumbbell_z",
+                    "max_roll_forearm","min_pitch_forearm","classe")
+  
+training_useful <- training[,useful_features]
+testing_useful <- testing[,useful_features]
+
+###### IMPUTE NA Values ######
+preObj <- preProcess(training_useful[,-22], method="knnImpute") 
+knnTrain <- predict(preObj, training_useful[,-22])
+knnTrain$classe <- training_useful$classe
+
+###### TRAIN ######
+tc = trainControl(method = "cv", number = 4)
+modelFit <- train(classe~.,
+                  method="rf", #ctree, rpart, gbm, rf --> (accuracy=60%),
+                  trControl = tc,
+                  prox = TRUE,
+                  allowParallel = TRUE,
+                  preProcess=c("pca"),
+                  data=knnTrain); modelFit  
 
 
-##### TRY2: Exploring Data (accuracy 70%)
-featurePlot(x=training[,10:14],
-            y=training$classe,
-            plot = "pairs")
-modelFit <- train(classe ~ pitch_belt + roll_belt + yaw_belt + total_accel_belt,
-                  method="ctree", #ctree, rpart, gbm, rf --> (accuracy=60%)
-                  data=training) 
-modelFit  
-
-##### TRY3: Exploring Data (accuracy 70%)
-featurePlot(x=training[,116:120],
-            y=training$classe,
-            plot = "pairs")
-# training$stddev_yaw_arm_log <- log10(training$stddev_yaw_arm+1) #dealing with the skewed distribution
-modelFit <- train(classe ~ pitch_belt + roll_belt + yaw_belt + total_accel_belt + 
-                    gyros_belt_y + gyros_belt_z + accel_belt_y +
-                    magnet_belt_y + roll_arm + pitch_arm + yaw_arm + avg_roll_arm + accel_arm_x +
-                    avg_roll_dumbbell + gyros_dumbbell_x + magnet_dumbbell_x + accel_dumbbell_y + 
-                    
-                    ,
-                  method="ctree", #ctree, rpart, gbm, rf --> (accuracy=60%),
-                  preProcess="pca",
-                  data=training); modelFit  
-
-
-###### testset ######
-confusionM <- confusionMatrix(predict(modelFit,testing), testing$classe)  
+###### TEST ######
+knnTest <- predict(preObj, testing_useful[,-22])
+confusionM <- confusionMatrix(predict(modelFit,knnTest), testing_useful$classe)  
 confusionM
 confusionM$table
 plot(confusionM$table)
+
+
+## Validation
+validating <- validationData[useful_features[-22]]
+knnValid <- predict(preObj, validating[,-22])
+answers = predict(modelFit,knnValid)
+
+## Submit Answers
+pml_write_files = function(x){
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+  }
+}
+pml_write_files(answers)
 
 
 
